092576c34bd375572a4c696c4735d4716486ce5a
Rokas Grigaravicius <grigroka@gmail.com>
2018-04-10 17:07:28 +0300

    made Category model

---
+++ app/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    public $fillable = ['title', 'parent_id'];
+
+    public function childs()
+    {
+        return $this->hasMany('App\Category', 'parent_id', 'id');
+    }
+}
