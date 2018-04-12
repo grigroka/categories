3fb011163eb5ff54574e4a57c70ab27adf9a328f
Rokas Grigaravicius <grigroka@gmail.com>
2018-04-12 10:56:26 +0300

    made categories view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,13 @@
-Hello world {{ $categories }}
+<div class="col-md-6">
+    <h3>Category List</h3>
+    <ul id="tree1">
+        @foreach($categories as $category)
+            <li>
+                {{ $category->title }}
+                @if(count($category->childs))
+                    @include('child',['childs' => $category->childs])
+                @endif
+            </li>
+        @endforeach
+    </ul>
+</div>
